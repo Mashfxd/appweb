65ea6310f6d9fb0061aa0f610e9b7c6c5042cd48
Mashfxd <68040014+Mashfxd@users.noreply.github.com>
2021-01-16 20:17:06 -0500

    Update RegistroCliente.php
    agregando la funcionalidad php para que registre a la base de datos

--- validar/RegistroCliente.php
+++ validar/RegistroCliente.php
@@ -1,2 +1,29 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+
+$conexion =mysqli_connect("localhost","id15811897_fernando","Monkey1999@@","id15811897_prestamosfinal",'3306');
+$idCliente=$_POST['cliente'];
+$nombreCliente = $_POST['NOMBRE'];
+$DniCliente = $_POST['DNI'];
+$DireccionCliente = $_POST['DIRECCION'];
+$telefonoCliente= $_POST["TELEFONO"];
+$correp = $_POST['correo'];
+$genero=$_POST['genero'];
+$edad=$_POST['edad'];
+
+$consulta="INSERT INTO `clientes` values ('$idCliente','$nombreCliente','$DniCliente','$DireccionCliente','$telefonoCliente','$correp','$genero','$edad')";
+$result= mysqli_query($conexion,$consulta);
+
+if(!$result){
+    echo "error al registrarse el cliente";
+   exit;
+  }else{
+    echo "cliente registrado correctamente";
+      exit;
+  }
+
+
+  mysqli_close($conexion);
+
 ?>
